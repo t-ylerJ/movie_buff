b52ebc77f3c7c256eec24294b2d4336445830eea
Tyler Johnson <tylerj0937@gmail.com>
2024-02-04 06:30:27 -0800

    big cast, it was ok, harrison ford

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -1,11 +1,12 @@
+
+
 def it_was_ok
   # Consider the following:
-  #
   # Movie.where(yr: 1970..1979)
-  #
   # You can use ranges (a..b) inside a where method.
-  #
   # Find the id, title, and score of all movies with scores between 2 and 3.
+  # Movie.select( :id, :title, :score).where(score: 2..3)
+  Movie.select('id, title, score').where('score BETWEEN 2 AND 3')
 
 end
 
@@ -21,6 +22,17 @@ def harrison_ford
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
 
+
+
+
+  #open source rb and look for classname/foreign key we want. 
+  #that relationship will be what we put in joins
+  Movie.joins(:actors, :castings)
+  .where(actors: {name: 'Harrison Ford'})
+  .where.not(castings: {ord: 1})
+  .select('DISTINCT movies.id, title')
+
+  # 
 end
 
 def biggest_cast
@@ -38,22 +50,32 @@ def biggest_cast
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
   
-end
+#always start with .rb file that has end result you want
+ Movie.joins(:castings).group('movies.id')
+    .order('COUNT(castings.id) DESC')
+    .limit(3)
+    .select('movies.id, movies.title')
+  #movie and not movie Id because association
 
+end
+ 
 def directed_by_one_of(them)
   # Consider the following:
-  #
   # Movie.where('yr IN (?)', years)
-  #
+
+  # given director names
   # You can use IN to test if an element is present in an array.
-  #
   # Active Record gives you an even better way to write this:
-  #
+
+    
   # Movie.where(yr: years)
+  #where(actors.name: ?, them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
   
   # Note: Directors appear in the 'actors' table.
+  Movie.joins(:director).where('actors.name IN (?)', them).select('movies.id, movies.title')
+  #director is assc, line 17
 
 end
 
